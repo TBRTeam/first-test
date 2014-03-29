d19c4b7c4c679775aa6b87ac094649e31b4e75cd
simone tolomelli <sasaki@MacBook-Pro-di-Simone.local>
2014-03-29 18:24:08 +0100

    first commit

---
+++ Test/TBRMini.h
@@ -0,0 +1,13 @@
+//
+//  TBRMini.h
+//  Test
+//
+//  Created by simone tolomelli on 29/03/14.
+//  Copyright (c) 2014 TBR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBRMini : NSObject
+
+@end
---
+++ Test/TBRMini.m
@@ -0,0 +1,13 @@
+//
+//  TBRMini.m
+//  Test
+//
+//  Created by simone tolomelli on 29/03/14.
+//  Copyright (c) 2014 TBR. All rights reserved.
+//
+
+#import "TBRMini.h"
+
+@implementation TBRMini
+
+@end
